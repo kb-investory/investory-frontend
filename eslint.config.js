d0c5150c2174272eef4aef52514c31a409d4c534
SangWoo <tkddn1012@gmail.com>
2026-07-22 16:27:57 +0900

    chore: ESLint 및 Prettier 설정 추가
    Vue 권장 린트와 공통 포맷 규칙을 적용
    CI 및 VS Code에서 동일한 규칙을 사용하도록 구성

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'vue/attributes-order': 'warn',
+      'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  eslintConfigPrettier,
+]
