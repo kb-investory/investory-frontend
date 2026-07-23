3aee14f1f6866fb68688d3ab698ed8fbab0e8b74
nsol0215 <nsol0215@gmail.com>
2026-07-23 14:11:28 +0900

    feat: 포트폴리오 계좌 요약 스토어 및 목 데이터 추가
    - 계좌 데이터(accountsMock) 및 요약 데이터(portfolioSummaryMock) 구현
    
    - portfolioStore에 계좌 선택 및 요약 데이터 조회 기능 추가

--- src/modules/portfolio/services/portfolioService.js
+++ src/modules/portfolio/services/portfolioService.js
@@ -1,8 +1,19 @@
 import {
+  accountsMock,
   portfolioAnalysisMock,
   portfolioMock,
+  portfolioSummaryMock,
   stockAnalysisMocks,
 } from '@/modules/portfolio/mocks/portfolioMock'
+
+export async function getAccounts() {
+  return structuredClone(accountsMock)
+}
+
+export async function getPortfolioSummary(accountId = 'ALL') {
+  const summary = portfolioSummaryMock[accountId] || portfolioSummaryMock.ALL
+  return structuredClone(summary)
+}
 
 export async function getPortfolioHoldings() {
   return structuredClone(portfolioMock)
